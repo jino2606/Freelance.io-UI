de21974fc3158b6343f725eb788df3b922ccffad
jino2606 <jinojino122@gmail.com>
2026-06-17 22:52:52 +0530

    Updated UI changes

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:4000'
-// export const BASE_URL = 'https://freelanceio-server.onrender.com'
+// export const BASE_URL = 'http://localhost:4000'
+export const BASE_URL = 'https://freelanceio-server.onrender.com'
 
 // API base includes /api prefix
 export const API_URL = `${BASE_URL}/api`
